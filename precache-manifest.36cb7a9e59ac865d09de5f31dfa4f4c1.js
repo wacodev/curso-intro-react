f5d67aa6884d9f92f1f061fd4ec28bab69726c7b
William Coto <wacodev@outlook.com>
2022-05-31 23:16:46 -0600

    Updates

---
+++ precache-manifest.36cb7a9e59ac865d09de5f31dfa4f4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40fa6c70f4b258c8f6d",
+    "url": "/curso-intro-react/static/css/main.3f850a44.chunk.css"
+  },
+  {
+    "revision": "d40fa6c70f4b258c8f6d",
+    "url": "/curso-intro-react/static/js/main.d40fa6c7.chunk.js"
+  },
+  {
+    "revision": "a7371354de14957d3644",
+    "url": "/curso-intro-react/static/js/1.a7371354.chunk.js"
+  },
+  {
+    "revision": "8f31ac8995903ac72b62",
+    "url": "/curso-intro-react/static/js/runtime~main.8f31ac89.js"
+  },
+  {
+    "revision": "a6ff2ce648ff3421426db7842d40d1d0",
+    "url": "/curso-intro-react/static/media/check.a6ff2ce6.svg"
+  },
+  {
+    "revision": "45d0d71a1f8f92c1da6744303a373163",
+    "url": "/curso-intro-react/static/media/delete.45d0d71a.svg"
+  },
+  {
+    "revision": "f572cc320f91ea83bdc8741e1c2691ad",
+    "url": "/curso-intro-react/index.html"
+  }
+];
